cca69aeb68107fcc23e613b0b3dee060f754311a
tanaybhartia <tanaybhartia@gmail.com>
2020-01-18 16:42:30 +0530

    js for verifying updated

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const { exec } = require("child_process");
+const fs = require('fs');
+var sha1 = require('sha1');
 
-exec("./verify pbc-0.5.14/param/a1.param ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad.signature artifacts 1 3 4", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
+const verifySig = (str) => {
+    const words = str.split('|');
+    const bin = Buffer.from(words[0], 'base64');
+    fs.writeFileSync('sha1.signature', bin);
+    const ip_sha = sha1(words[1]);
+    var exec_str = "./verify pbc-0.5.14/param/a1.param " + ip_sha + " sha1.signature artifacts";
+    for (let i = 2; i < words.length; i++) {
+        exec_str = exec_str + " " + words[i];
     }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
+    console.log(exec_str);
+    exec(exec_str, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
+
+module.exports = verifySig;
